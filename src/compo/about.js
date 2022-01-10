5629a4a566d1304fc822d0c03ac2967bba194fa5
anujgiri20 <87684592+anujgiri20@users.noreply.github.com>
2022-01-10 16:41:32 +0700

    first commit

--- src/compo/about.js
+++ src/compo/about.js
@@ -10,14 +10,11 @@ function About()
       <div className="about_div">
         <div className="about_inner_div">
             <h1 className="about_content">What is the purpose of Google Keeper?
-Google Keeper allows users to record plain-text notes and organize, edit, or share them with others using a suite of collaboration tools. You can also use new version of Google Keeper to create voice notes, or set time- and location-based reminders.Following are some key features of Google kepper</h1>
+Google Keeper allows users to record plain-text notes and organize, edit, or delete the notes along with login and jwt-authentication for user privacy.Following are some key features of Google kepper</h1>
         </div>
         <div className="outer">
               <div  className="inner">
-                  <h1>SignIn/SignUp</h1>
-              </div>
-              <div className="inner">
-                  <h1>Two step verification</h1>
+                  <h1>SignIn/SignUp/jwt-auth</h1>
               </div>
               <div className="inner">
                   <h1>CRUD operation</h1>
